7af93118e73b9bf8d2976b34b37d1e686f7fedd8
FelipeReyesMi <89532512+FelipeReyesMi@users.noreply.github.com>
2021-12-18 15:17:52 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector(".menuboton").addEventListener("click",() =>{
+    document.querySelector(".nav-submenu1").classList.toggle("show")
+});
+
+ScrollReveal().reveal('.noticiaPrincipal');
+ScrollReveal().reveal('.cards', { delay: 500 });
+ScrollReveal().reveal('.seccion', { delay: 500 });
+ScrollReveal().reveal('.seccion2', { delay: 500 });
+ScrollReveal().reveal('.piepagina', { delay: 500 });
+ScrollReveal().reveal('.nota', { delay: 500 });
