e770cf278b30bcd8e831c6985393735da600fce4
FelipeReyesMi <89532512+FelipeReyesMi@users.noreply.github.com>
2021-12-18 18:50:43 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ document.querySelector(".menuboton").addEventListener("click",() =>{
 });
 
 ScrollReveal().reveal('.noticiaPrincipal');
-ScrollReveal().reveal('.cards', { delay: 500 });
-ScrollReveal().reveal('.seccion', { delay: 500 });
-ScrollReveal().reveal('.seccion2', { delay: 500 });
-ScrollReveal().reveal('.piepagina', { delay: 500 });
-ScrollReveal().reveal('.nota', { delay: 500 });
+ScrollReveal().reveal('.cards', { delay: 550 });
+ScrollReveal().reveal('.seccion', { delay: 550 });
+ScrollReveal().reveal('.seccion2', { delay: 550 });
+ScrollReveal().reveal('.piepagina', { delay: 550 });
+ScrollReveal().reveal('.nota', { delay: 550 });
+ScrollReveal().reveal('.social', { delay: 550 });
+ScrollReveal().reveal('.letra', { delay: 550 });
+ScrollReveal().reveal('.imagentravel', { delay: 550 });
+ScrollReveal().reveal('.textopri', { delay: 550 });
+ScrollReveal().reveal('.integrantes', { delay: 550 });
